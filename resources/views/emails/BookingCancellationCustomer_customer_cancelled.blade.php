5d9a2ae10afc4c3a6a6df2458b3d26f8b0bd2a15
mdanikhan11 <m.daniyal.khan6@gmail.com>
2021-05-27 16:10:38 +0500

    email templete

--- resources/views/emails/BookingCancellationCustomer_customer_cancelled.blade.php
+++ resources/views/emails/BookingCancellationCustomer_customer_cancelled.blade.php
@@ -416,7 +416,7 @@ width: 100% !important;
 
 <td valign="top" style="padding:5px;"><div style="font-family:Cabin, sans-serif;font-size:17px;color:#5d4e6d;line-height:25px;letter-spacing: 0.03em;text-align:left;"><p style="padding: 0; margin: 0;"><span class="mso-font-fix-arial">BOOKING LOCATION:&nbsp;</span></p>
 
-<p style="padding: 0; margin: 0;"><span class="mso-font-fix-arial"><strong>{{$order->booker->user_address->street}}, {{$order->booker->user_address->suburb}}, {{$order->booker->user_address->city}}, {{$order->booker->user_address->state}}, {{$order->booker->user_address->postcode}}, {{$order->booker->user_address->country->country}}</strong></span></p>
+<p style="padding: 0; margin: 0;"><span class="mso-font-fix-arial"><strong>{{@$order->booker->user_address->street}}, {{@$order->booker->user_address->suburb}}, {{@$order->booker->user_address->city}}, {{@$order->booker->user_address->state}}, {{@$order->booker->user_address->postcode}}, {{@$order->booker->user_address->country->country}}</strong></span></p>
 </div>
 </td>
 </tr>
