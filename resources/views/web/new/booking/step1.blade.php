e58da926f23207d54e9df4bc90ba5ad53d0178c4
captainwasi123 <captain.wasi@gmail.com>
2021-09-15 09:03:47 -0700

    Update 15-09-2021

--- resources/views/web/new/booking/step1.blade.php
+++ resources/views/web/new/booking/step1.blade.php
@@ -64,10 +64,21 @@
                                        @foreach($val->availability as $avail)
                                           @if(ucfirst($avail->week_day) == $day)
                                              @foreach($avail->slots as $slot)
+                                                @php
+                                                   $x = 0;
+                                                   $duration = 30; 
+                                                   $buffer = 120; 
+                                                   $start = $slot->start_booking; 
+                                                   $end = $slot->end_booking; 
+                                                   $end = date('H:i:s',strtotime('-'.$buffer.' minutes',strtotime($end)));
+                                                @endphp
+                                                @while($start <= $end)
                                                 <div>
-                                                <input type="radio" id="myCheck{{$slot->id}}" class="timeslot" name="timeslot" data-time="{{date('h:i A', strtotime($slot->start_booking))}}" data-prac="{{base64_encode($val->id)}}" tabindex="-1"> 
-                                                <label class="book-time-btn"  for="myCheck{{$slot->id}}" >{{date('h:i A', strtotime($slot->start_booking))}} </label>    
-                                                </div>                    
+                                                   <input type="radio" id="myCheck{{$slot->id.$x}}" class="timeslot" name="timeslot" data-time="{{date('h:i A', strtotime($start))}}" data-prac="{{base64_encode($val->id)}}" tabindex="-1"> 
+                                                   <label class="book-time-btn"  for="myCheck{{$slot->id.$x}}" >{{date('h:i A', strtotime($start))}}</label>
+                                                </div>
+                                                   @php $start = date('H:i:s',strtotime('+'.$duration.' minutes',strtotime($start))); $x++; @endphp
+                                                @endwhile 
                                              @endforeach
                                           @endif
                                        @endforeach
