d1251f95677e78afee7e1fac574300ff8805d8bb
anasQamarWork <anasqamar.work@gmail.com>
2022-02-18 19:03:52 +0500

    Anas 18-02-2022

--- resources/views/web/new/index.blade.php
+++ resources/views/web/new/index.blade.php
@@ -11,6 +11,7 @@
               <div class="banner-text">
                  <h3> All your favourite treatments at home
                  </h3>
+                 <a href="" data-toggle="modal" data-target=".safe-treatment-popup">New Popup</a>
                  
                  <p> Wellbe provides beauty & wellness services in the comfort of your home with therapists you can trust.  </p>
                  <form method="get" id="treatmentForm" action="{{route('treatments.search')}}">
